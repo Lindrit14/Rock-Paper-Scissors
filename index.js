36f06c33f34c101f0d15da309cd70aa34bd1d2d2
Lindrit14 <Lindrit_lindi@hotmail.com>
2022-01-26 00:32:39 +0100

    make code cleaner and make the game() work/ still just 5 repetions. Need to maake enough repetions so that draws dont count

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// the array of moves the computer can choose from
-let moves = ["rock", "paper", "sciccors"]
-
-// defining how the input of the player should be taken. and what should be done to it
-let playerSelection = prompt("Choose a move").toLowerCase();
-
-//defining how the computer gets its move
-let computerSelection = computerPlay();
-
-let playerScore = 0
-
-let computerScore = 0
 
 // this is how the computer chooses a random move from move 
 function computerPlay(){
@@ -17,64 +5,96 @@ function computerPlay(){
     return move
 }
 // here are all the possible outcomes. We get ONE outcome if one case is true. 
-function playRPS(playerSelection, computerSelection){
-
-switch (true) {
+function playRound(playerSelection, computerSelection){
 
 //this section is for rock possabilities///////////////////////////////
-    case computerSelection === moves[0] && playerSelection === moves[0]:
-        return "Draw"
-    case computerSelection === moves[0] && playerSelection === moves[1]:
-        console.log(`Player wins! ${moves[1]} beats ${moves[0]}`)
-        return ++playerScore 
-        
-    case computerSelection === moves[0] && playerSelection === moves[2]:
-        console.log(`Computer wins! ${moves[0]} beats ${moves[2]}`)
-        return ++computerScore
-        
+    if(computerSelection === moves[0] && playerSelection === moves[0]){
+        return "Draw";
+
+    }else if(computerSelection === moves[0] && playerSelection === moves[1]){
+        console.log(`Player wins! ${moves[1]} beats ${moves[0]}`);
+        return"Player Score: " + ++playerScore;
+         
+    }else if(computerSelection === moves[0] && playerSelection === moves[2]){
+        console.log(`Computer wins! ${moves[0]} beats ${moves[2]}`);
+        return "Computer Score: " + ++computerScore;
 
 //this section is for paper possabilities/////////////////////////
-    case computerSelection === moves[1] && playerSelection === moves[1]:
-        return "Draw"
-    case computerSelection === moves[1] && playerSelection === moves[2]:
-        console.log(`Player wins! ${moves[2]} beats ${moves[1]}`)
-        return ++playerScore
-        
-    case computerSelection === moves[1] && playerSelection === moves[0]:
-        console.log(`Computer wins! ${moves[1]} beats ${moves[0]}`)
-        return ++computerScore 
+    }else if(computerSelection === moves[1] && playerSelection === moves[1]){
+        return "Draw";
+
+    }else if(computerSelection === moves[1] && playerSelection === moves[2]){
+        console.log(`Player wins! ${moves[2]} beats ${moves[1]}`);
+        return "Player Score: " + ++playerScore;
+
+    }else if(computerSelection === moves[1] && playerSelection === moves[0]){
+        console.log(`Computer wins! ${moves[1]} beats ${moves[0]}`);
+        return "Computer Score: " + ++computerScore;
 
 //this section is for sciccors possabilities ///////////////////////////
-    case computerSelection === moves[2] && playerSelection === moves[2]:
-        return "Draw"
-    case computerSelection === moves[2] && playerSelection === moves[0]:
-        console.log(`Player wins! ${moves[0]} beats ${moves[2]}`)
-        return ++playerScore 
-          
-    case computerSelection === moves[2] && playerSelection === moves[1]:
-        console.log(`Computer wins! ${moves[2]} beats ${moves[1]}`)
-        return ++computerScore
+    }else if(computerSelection === moves[2] && playerSelection === moves[2]){
+        return "Draw";
 
-    default: console.log("Suck Some Dick Lind")
-        break;
+    }else if(computerSelection === moves[2] && playerSelection === moves[0]){
+        console.log(`Player wins! ${moves[0]} beats ${moves[2]}`);
+        return "Player Score: " + ++playerScore;
+        
+
+    }else if(computerSelection === moves[2] && playerSelection === moves[1]){
+        console.log(`Computer wins! ${moves[2]} beats ${moves[1]}`);
+        return "Computer Score: " + ++computerScore;
+
+    }else {
+        return console.log("Error");
     }
+    
 }
-// this is function for executing playRPS 5 times. Problem: it doesnt go up to 5 point for either player
+
 function game(){
-    for (let i = 1; i <= 5; i++) {
-        console.log(playRPS(playerSelection, computerSelection))
-        playerSelection = prompt("Choose a move").toLowerCase();
-        computerSelection = computerPlay()
-        if(computerScore === 5){
-            console.log("Computer WINS")
-        }else if(playerScore === 5){
-            console.log("Player WINS")
-        }
+
+    for(let i=1;i<= 5; i++){
+        console.log(playRound(playerSelection, computerSelection));
+        playerSelection = prompt("Choose Again:").toLowerCase();
+        computerSelection = computerPlay();  
     }
-    return "END OF THE GAME";
+    if(playerScore > computerScore){
+        console.log("Player Won");
+    }else if(playerScore === computerScore){
+        console.log("It's a Draw");
+    }else{
+        console.log("Computer Won");
+    }
+
+    //Play the game as many times as it takes the player or the computer to reach a score of 5
+    //after every round the player and the computer need to choose their mooves again
+    //  
+
+return "Game has ended";
 }
+
+
+
+
+// the array of moves the computer can choose from
+let moves = ["rock", "paper", "sciccors"];
+
+// defining how the input of the player should be taken. and what should be done to it
+let playerSelection = prompt("Choose a moove").toLocaleLowerCase();
+
+//defining how the computer gets its move
+let computerSelection = computerPlay();
+
+
+let playerScore = 0;
+
+let computerScore = 0;
+
 
 console.log(game())
+
+
+
+
 
 
 
